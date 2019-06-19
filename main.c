623fea9e4502962a877ca94e1acc66cb02ce770a
Poly000 <1348292515@qq.com>
2019-06-19 13:13:11 +0800

    占用单字节

--- main.c
+++ main.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
 int main(int argc, char **argv) {
-	register int i=0,j,k,l,m;
+	register int i=0,j,k,m;
 	if (argc!=2) {
 		printf("Usage: %s [string]",*argv);
 		return 1;
 	}
 	while (argv[1][++i]!=0);
-	for (j=0; j<i; j++) {
-		l=k=argv[1][j];
-		k>>=3;
-		l&=~(k<<3);
-		if (k!=0) {
-			if (k==1)
-				printf("++++++++");
-			else {
-				printf("++++++++[>");
-				for (m=0; m<k; m++)
-					putchar('+');
-				printf("<-]>");
-			}
-		}
-		for (m=0; m<l; m++)
-			putchar('+');
+	for (j=0; j<argv[1][0]; j++)
+		putchar('+');
+	for (j=1; j<i; j++) {
+		k=argv[1][j]-argv[1][j-1];
+		if (k>0)
+			for (m=k; m>0; m--)
+				putchar('+');
+		else
+			for (m=k; m<0; m++)
+				putchar('-');
 		putchar('.');
-		if (j!=i-1)
-			printf("[-]");
 	}
 	return 0;
 }
