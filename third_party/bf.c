2a7957616d70963e169f45b2f4916028c7b93749
poly000 <1348292515@qq.com>
2020-03-09 23:55:35 +0800

    emm

--- third_party/bf.c
+++ third_party/bf.c
@@ -1,5 +1,24 @@
 /*
  Brainfuck-C ( http://github.com/kgabis/brainfuck-c )
+ Copyright (c) 2012 Krzysztof Gabis
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
 */
 
 #include <stdio.h>
@@ -25,27 +44,6 @@
 #define STACK_POP()     (STACK[--SP])
 #define STACK_EMPTY()   (SP == 0)
 #define STACK_FULL()    (SP == STACK_SIZE)
-
-#define LICENSE \
-" Copyright (c) 2012 Krzysztof Gabis\n"\
-" \n"\
-" Permission is hereby granted, free of charge, to any person obtaining a copy\n"\
-" of this software and associated documentation files (the \"Software\"), to deal\n"\
-" in the Software without restriction, including without limitation the rights\n"\
-" to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"\
-" copies of the Software, and to permit persons to whom the Software is\n"\
-" furnished to do so, subject to the following conditions:\n"\
-" \n"\
-" The above copyright notice and this permission notice shall be included in\n"\
-" all copies or substantial portions of the Software.\n"\
-" \n"\
-" THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"\
-" IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"\
-" FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"\
-" AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"\
-" LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"\
-" OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n"\
-" THE SOFTWARE.\n"
 
 struct instruction_t {
     unsigned short operator;
@@ -120,7 +118,7 @@ int main(int argc, const char * argv[])
     int status;
     FILE *fp;
     if (argc != 2 || (fp = fopen(argv[1], "r")) == NULL) {
-        fprintf(stderr, "Usage: %s filename\n----------------\nLicense:\n" LICENSE, argv[0]);
+        fprintf(stderr, "Usage: %s filename\n", argv[0]);
         return FAILURE;
     }
     status = compile_bf(fp);
@@ -131,6 +129,5 @@ int main(int argc, const char * argv[])
     if (status == FAILURE) {
         fprintf(stderr, "Error!\n");
     }
-	fprintf(stderr, "\n----------------\nLicense:\n" LICENSE);
     return status;
 }
