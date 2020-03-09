beb36126266794bf493fb9672a2904f8ea00eb52
poly000 <1348292515@qq.com>
2020-03-09 11:00:39 +0800

    a

--- main_speed.c
+++ main_speed.c
@@ -1,49 +1,69 @@
 #include <stdio.h>
+// #define DEBUG
+
+int _Abs(register int a, register int b) {
+	register int t = a-b;
+	a = t < 0 ? -t : t;
+	return a;
+}
 
 int main(int argc, char **argv) {
-	register int i = 0,j,k,l,m;
+	register int i = 0,j,k,l,m,last = 0;
 	if (argc != 2) {
 		printf("Usage: %s [string]",*argv);
 		return 1;
 	}
 	while (argv[1][++i]!=0)
 		continue;
-	--i;
 	for (j=0; j<i; j++) {
-		l = k = argv[1][j];
-		k >>= 3;
-		l &= (1<<k)+1;
-		if (k != 0) {
+		register int t;
+		t = argv[1][j];
+		l = k = t-last;
+		last = t;
+#ifdef DEBUG
+		printf("\n%d, %d, %d, %d\n",last,k,j,i);
+#endif
+
+		if (k >= 0) {
+			k >>= 3; // k /= 8
+			l &= 7; //在更多的机器工作，求余
 			if (k == 1)
-				printf("++++++++");
+				printf(">++++++++");
+			else {
+				if (k != 0) {
+					printf("++++++++[>");
+					for (m=0; m<k; m++)
+						putchar('+');
+					printf("<-]>");
+				} else {
+
+				}
+			}
+			for (m=0; m<l; m++)
+				putchar('+');
+			putchar('.');
+		} else {
+			k = -k;
+			l = -l;
+			k >>= 3; // k /= 8
+			l &= 7; //在更多的机器工作，求余
+			if (k == 1)
+				printf(">--------");
 			else {
 				printf("++++++++[>");
 				for (m=0; m<k; m++)
-					putchar('+');
+					putchar('-');
 				printf("<-]>");
 			}
+			for (m=0; m<l; m++)
+				putchar('-');
+			putchar('.');
 		}
-		for (m=0; m<l; m++)
-			putchar('+');
-		putchar('.');
-		putchar('>');
+#ifdef DEBUG
+		printf("\n%d, %d, %d\n",m,k,l);
+#endif
+
+		putchar('<');
 	}
-	l = k = argv[1][i];
-	k >>= 3;
-	l &= (1<<k)+1;
-	if (k != 0) {
-		if (k == 1)
-			printf("++++++++");
-		else {
-			printf("++++++++[>");
-			for (m=0; m<k; m++)
-				putchar('+');
-			printf("<-]>");
-		}
-	}
-	for (m=0; m<l; m++)
-		putchar('+');
-	putchar('.');
-	return 0;
+
 }
-// 需要优化
